f487c402af150913be6b7db44a10c4ade1ef6a3a
tjgeass <tjgeass@qq.com>
2019-06-18 15:36:46 +0800

    图表页及增加图标库
    Signed-off-by: tjgeass <tjgeass@qq.com>

--- src/renderer/views/device/chart.vue
+++ src/renderer/views/device/chart.vue
@@ -46,7 +46,10 @@ export default {
         dev_id: this.$route.query.dev_id,
         dateCate: 3
       },
-      option: {
+      /**
+       * 倾角配置
+       */
+      optionType1: {
         legend: {},
         tooltip: {
           trigger: "axis",
@@ -88,19 +91,13 @@ export default {
           {
             left: "10%",
             right: "8%",
-            height: "20%"
+            height: "30%"
           },
           {
             left: "10%",
             right: "8%",
-            top: "36%",
-            height: "16%"
-          },
-          {
-            left: "10%",
-            right: "8%",
-            top: "62%",
-            height: "16%"
+            top: "48%",
+            height: "30%"
           }
         ],
         // 声明一个 X 轴，类目轴（category）。默认情况下，类目轴对应到 dataset 第一列。
@@ -124,11 +121,6 @@ export default {
                 show: false
               }
             }
-          },
-          {
-            gridIndex: 2,
-            scale: true,
-            type: "category"
           }
         ],
         // 声明一个 Y 轴，数值轴。
@@ -159,27 +151,18 @@ export default {
             axisLabel: {
               formatter: "{value} °"
             }
-          },
-          {
-            name: "裂隙",
-            scale: true,
-            gridIndex: 2,
-            splitNumber: 2,
-            axisLabel: {
-              formatter: "{value} mm"
-            }
           }
         ],
         dataZoom: [
           {
             type: "inside",
-            xAxisIndex: [0, 1, 2],
+            xAxisIndex: [0, 1],
             start: 50,
             end: 100
           },
           {
             show: true,
-            xAxisIndex: [0, 1, 2],
+            xAxisIndex: [0, 1],
             type: "slider",
             top: "85%",
             start: 50,
@@ -209,7 +192,10 @@ export default {
             encode: {
               y: ["humi"]
             },
-            yAxisIndex: 1
+            yAxisIndex: 1,
+            itemStyle: {
+              color: "#91c7ae"
+            }
           },
           {
             name: "前后倾角",
@@ -236,6 +222,159 @@ export default {
               show: true
             },
             showAllSymbol: false
+          }
+        ]
+      },
+      /**
+       * 裂隙配置
+       */
+      optionType2: {
+        legend: {},
+        tooltip: {
+          trigger: "axis",
+          axisPointer: {
+            type: "cross"
+          }
+        },
+        axisPointer: {
+          link: { xAxisIndex: "all" },
+          label: {
+            backgroundColor: "#777"
+          }
+        },
+        toolbox: {
+          feature: {
+            dataZoom: {
+              yAxisIndex: false
+            },
+            brush: {
+              type: ["lineX", "clear"]
+            }
+          }
+        },
+        dataset: {
+          // 提供一份数据。
+          dimensions: [
+            "uplink_at",
+            "temp",
+            "humi",
+            "illu",
+            "alti",
+            "perc",
+            "dire_0",
+            "dire_1"
+          ],
+          source: []
+        },
+        grid: [
+          {
+            left: "10%",
+            right: "8%",
+            height: "30%"
+          },
+          {
+            left: "10%",
+            right: "8%",
+            top: "48%",
+            height: "30%"
+          }
+        ],
+        // 声明一个 X 轴，类目轴（category）。默认情况下，类目轴对应到 dataset 第一列。
+        xAxis: [
+          {
+            gridIndex: 0,
+            show: false,
+            type: "category",
+            axisPointer: {
+              label: {
+                show: false
+              }
+            }
+          },
+          {
+            gridIndex: 1,
+            scale: true,
+            type: "category",
+            axisPointer: {
+              label: {
+                show: false
+              }
+            }
+          }
+        ],
+        // 声明一个 Y 轴，数值轴。
+        yAxis: [
+          {
+            name: "温度°C",
+            scale: true,
+            position: "left",
+            gridIndex: 0,
+            axisLabel: {
+              formatter: "{value} °C"
+            }
+          },
+          {
+            name: "湿度%Rh",
+            position: "right",
+            scale: true,
+            gridIndex: 0,
+            axisLabel: {
+              formatter: "{value} %Rh"
+            }
+          },
+          {
+            name: "裂隙",
+            scale: true,
+            gridIndex: 1,
+            splitNumber: 1,
+            axisLabel: {
+              formatter: "{value} mm"
+            }
+          }
+        ],
+        dataZoom: [
+          {
+            type: "inside",
+            xAxisIndex: [0, 1],
+            start: 50,
+            end: 100
+          },
+          {
+            show: true,
+            xAxisIndex: [0, 1],
+            type: "slider",
+            top: "85%",
+            start: 50,
+            end: 100
+          }
+        ],
+        // 声明多个 bar 系列，默认情况下，每个系列会自动对应到 dataset 的每一列。
+        series: [
+          {
+            name: "温度",
+            type: "line",
+            smooth: true,
+            encode: {
+              y: ["temp"]
+            },
+            smooth: true,
+            label: {
+              show: true
+            },
+            showAllSymbol: false
+          },
+          {
+            name: "湿度",
+            type: "bar",
+            large: true,
+            barMaxWidth: 10,
+            encode: {
+              y: ["humi"]
+            },
+            yAxisIndex: 1,
+            itemStyle: {
+              color: "#91c7ae"
+            }
           },
           {
             name: "裂隙",
@@ -243,8 +382,8 @@ export default {
             encode: {
               y: ["perc"]
             },
-            xAxisIndex: 2,
-            yAxisIndex: 3,
+            xAxisIndex: 1,
+            yAxisIndex: 2,
             label: {
               show: true
             },
@@ -260,6 +399,14 @@ export default {
   computed: {
     device: function() {
       return this.$store.getters.getDeviceById(this.dataQuery.dev_id);
+    },
+    option: function() {
+      if (this.device.type_id == 3) {
+        return this.optionType1;
+      }
+      if (this.device.type_id == 4) {
+        return this.optionType2;
+      }
     }
   },
   watch: {
